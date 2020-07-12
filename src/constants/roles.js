8f59baf190e4f366fc9ae930e6578789a806addb
mastprogram <amol@Kacchuas-MBP.localdomain>
2020-07-11 18:01:46 -0700

    Revert "Creating Full Template"
    This reverts commit c195cf35b6d87ccf3f9dfe70b5e2c3c3ff9e1291.

--- src/constants/roles.js
+++
@@ -1,9 +0,0 @@
-/*
-  * These are the four predefined roles - admin, mod, student, and user. In order to change the default user to student, go to SignUp and change that initialization.
-  * However, note that you should follow the instructions in SignUp.
-*/
-
-export const ADMIN = 'ADMIN';
-export const MOD = 'MOD';
-export const STUDENT = 'STUDENT';
-export const USER = 'USER';
